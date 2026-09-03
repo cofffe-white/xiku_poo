d24e9931be565ebfc49bef4aa66efc58e5f19c71
calangoqwert <chilledosolv@gmail.com>
2026-09-03 10:15:05 -0300

    vini xico

--- ai.py
+++ ai.py
@@ -1,1 +1,7 @@
-print('aipim')
+nome = 'chico'
+idade = 13
+aluno = True
+if aluno is True:
+    print(f'O aluno {nome} tem {idade} anos.')
+else:
+    print(f'O aluno {nome} não está matriculado.')
