3557b3046eb106e27fe409a59720242964cca995
calangoqwert <chilledosolv@gmail.com>
2026-09-04 09:00:04 -0300

    BBE

--- ai.py
+++ ai.py
@@ -1,7 +1,31 @@
-nome = 'chico'
-idade = 13
-aluno = True
-if aluno is True:
-    print(f'O aluno {nome} tem {idade} anos.')
-else:
-    print(f'O aluno {nome} não está matriculado.')
+def o():
+    class clt:
+        def __init__(self, nome):
+            self.nome = nome
+        def trabalhar(self):
+            print(f"{self.nome} está trabalhando.")
+
+    class dev:
+        def __init__(self, nome):
+            self.nome = nome
+        def trabalhar(self):
+            print(f'{self.nome} está desenvolvendo.')
+
+    def ato(people):
+        people.trabalhar()
+
+    gusta = clt("Gustavo")
+    edi = dev("Edi")
+
+    ato(edi)
+    ato(gusta)
+
+#polimorfismo built-ins e operadores
+
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+    def falar(self):
+        return f'{self,nome} faz som'
+class 
+cão = Animal('Rex')
