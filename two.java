825e6a6cbf8550c51cf5d4acf0a522b50ee95671
calangoqwert <chilledosolv@gmail.com>
2026-09-04 07:37:26 -0300

    javão

--- two.java
+++ two.java
@@ -3,7 +3,7 @@ public class two {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("me diga um numero entre 10 e 20: ");
-        Int r = s.nextInt();
+        int r = s.nextInt();
         if(r > 10 && r < 20){
             System.out.println("numero certo");
         }
