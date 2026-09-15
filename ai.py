3d4b39ee4aa93d9fe014dd6689df1675400ce7b4
calangoqwert <chilledosolv@gmail.com>
2026-09-15 09:08:16 -0300

    acerto

--- ai.py
+++ ai.py
@@ -64,26 +64,62 @@ fusca = Carro("Fusca")
 honda = Moto("Honda")
 azul = Aviao("Azul")  
      
-class comida:
+import time
+
+class restaurante:
     def __init__(self,nome):
             self.nome = nome
-    def comer(self):
-            return f"{self.nome} está sendo devorada"
-
-class pera(comida):
-    def comer(self):
-            return f"{self.nome} está sendo devorada como uma pera"
-class maca(comida):
-    def comer(self):
-            return f"{self.nome} está sendo devorada como uma maçã"
-class banana(comida):
-    def comer(self):
-            return f"{self.nome} está sendo devorada como uma banana"
+    def pedir(self):
+        return f"{self.nome} seu pedido está a caminho"
+        time.sleep(10)
+        print("seu pedido chegou. Aproveite!")
         
-def ato(fruta):
-        return fruta.comer()
-maca = maca("Maçã")
-banana = banana("Banana")
-pera = pera("Pera")
+class cafeteria(restaurante):
+    def pedir(self):
+        print("seu pedido está a caminho")
+        time.sleep(10)
+        print("seu pedido chegou. Aproveite!")
+        
+    def cardapio(self):
+        e = input('bem-vindo ao cardápio da cafeteria:\n1-expresso\n2-capuccino\n3-café com leite\n4-café gelado\n5-americano. \nDigite o que deseja:\n ')
+            
+class acaiteria(restaurante):
+    def pedir(self):
+        print("seu pedido está a caminho")
+        time.sleep(10)
+        print("seu pedido chegou. Aproveite!")
+        
+    def cardapio(self):
+        s = input('bem-vindo ao cardápio da acaiteria,:\n1-acai com granola\n2-acai com banana\n3-acai com morango\n4-acai com leite condensado\n5-acai com chocolate. \nDigite o que deseja:\n ')
+                
+class quiosque(restaurante):
+    def pedir(self):
+        print("seu pedido está a caminho")
+        time.sleep(10)
+        print("seu pedido chegou. Aproveite!")
+    def cardapio(self):
+        c = input('bem-vindo ao cardápio do quiosque,:\n1-sorvete\n2-picolé\n3-batata frita\n4-cachorro quente\n5-hamburguer. \nDigite o que deseja:\n')
+                
+        
+def ato(pedido):
+        return pedido.pedir()
     
-print(f"{ato(maca)},\n {ato(banana)},\n {ato(pera)}")
+def acao(esc):
+    return esc.cardapio()
+    
+cafe1 = cafeteria('thalita paes')
+acai1 = acaiteria('lemania')
+quio1 = quiosque('comebem')
+
+p = input('bem-vindo ao restaurante, qual deseja ir?\n1-cafeteria\n2-acaiteria\n3-quiosque\nDigite o que deseja:\n')
+p = int(p)
+if p == 1:
+    print(acao(cafe1))
+    print(ato(cafe1))
+elif p == 2:
+    print(acao(acai1))
+elif p ==3:
+    print(acao(quio1))
+else:
+    print('opção inválida')
+
