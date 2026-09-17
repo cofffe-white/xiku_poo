2a8c8e8773a04cf1ed9c3eb5f0a066091738e57f
calangoqwert <chilledosolv@gmail.com>
2026-09-17 07:47:03 -0300

    MEU PROJETIN LINDO

--- ai.py
+++ ai.py
@@ -64,28 +64,39 @@ fusca = Carro("Fusca")
 honda = Moto("Honda")
 azul = Aviao("Azul")  
      
+#restaurante
 import time
 
 class restaurante:
     def __init__(self,nome):
-            self.nome = nome
+        
+        self.nome = nome
+        
     def pedir(self):
-        return f"{self.nome} seu pedido está a caminho"
+        print("seu pedido está a caminho")
+        
+    def time(self):
         time.sleep(10)
         print("seu pedido chegou. Aproveite!")
         
 class cafeteria(restaurante):
+    
     def pedir(self):
-        print("seu pedido está a caminho")
+            print("seu pedido está a caminho")
+            
+    def time(self):
         time.sleep(10)
         print("seu pedido chegou. Aproveite!")
-        
+            
     def cardapio(self):
         e = input('bem-vindo ao cardápio da cafeteria:\n1-expresso\n2-capuccino\n3-café com leite\n4-café gelado\n5-americano. \nDigite o que deseja:\n ')
             
 class acaiteria(restaurante):
+    
     def pedir(self):
         print("seu pedido está a caminho")
+        
+    def time(self):
         time.sleep(10)
         print("seu pedido chegou. Aproveite!")
         
@@ -93,10 +104,14 @@ class acaiteria(restaurante):
         s = input('bem-vindo ao cardápio da acaiteria,:\n1-acai com granola\n2-acai com banana\n3-acai com morango\n4-acai com leite condensado\n5-acai com chocolate. \nDigite o que deseja:\n ')
                 
 class quiosque(restaurante):
+    
     def pedir(self):
         print("seu pedido está a caminho")
+    
+    def time(self):    
         time.sleep(10)
         print("seu pedido chegou. Aproveite!")
+    
     def cardapio(self):
         c = input('bem-vindo ao cardápio do quiosque,:\n1-sorvete\n2-picolé\n3-batata frita\n4-cachorro quente\n5-hamburguer. \nDigite o que deseja:\n')
                 
@@ -106,6 +121,9 @@ def ato(pedido):
     
 def acao(esc):
     return esc.cardapio()
+
+def car(dapio):
+    return dapio.time()
     
 cafe1 = cafeteria('thalita paes')
 acai1 = acaiteria('lemania')
@@ -113,13 +131,18 @@ quio1 = quiosque('comebem')
 
 p = input('bem-vindo ao restaurante, qual deseja ir?\n1-cafeteria\n2-acaiteria\n3-quiosque\nDigite o que deseja:\n')
 p = int(p)
+
 if p == 1:
     print(acao(cafe1))
     print(ato(cafe1))
+    print(car(cafe1))
 elif p == 2:
     print(acao(acai1))
+    print(ato(acai1))
+    print(car(acai1))
 elif p ==3:
     print(acao(quio1))
+    print(ato(quio1))
+    print(car(quio1))
 else:
     print('opção inválida')
-
